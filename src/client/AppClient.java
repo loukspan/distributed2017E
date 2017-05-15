540d4e0c0c81f6f83ec5c3f8360529db6477312c
amoraitis <tasosthira7@gmail.com>
2017-05-15 15:31:54 +0300

    Directions deserialization implemented: duration, steps e.t.c.

--- src/client/AppClient.java
+++ src/client/AppClient.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.net.*;
 import java.nio.file.*;
 import java.sql.*;
+import java.util.Map;
+
 import master.Master;
 import queryres.CreateQuery;
 import workers.*;
@@ -13,7 +15,8 @@ import model.*;
 public class AppClient {
 	private static java.util.Scanner scanner = new java.util.Scanner(System.in);
 	public static void main(String[] args){
-	    masterMain();
+	    testing();
+		//masterMain();
 		//reducerMain();
 		//clientMain();
 		//workerMain();
@@ -75,7 +78,14 @@ public class AppClient {
 	    }
 		
 	}
-	
+	private static void testing() {
+		Directions ourDirections;
+		Map<Integer, Directions> mappedDirections;
+		MapWorker mapWorker = new MapWorker();
+		mappedDirections = mapWorker.map();
+		ourDirections = mappedDirections.get(0);
+		System.out.println(ourDirections.getSteps().get(0).getDistance());
+	}
 	
 	/*
 	 * FIXME: Separate to 3 functions connect, insert data and query
